c64e3e7c2d17e5cdf44b5d9574fba0462a0e5f46
Raymundo Pulido <raymundo.pulido.beja@gmail.com>
2024-08-17 15:14:17 -0600

    feat: remove error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from chalice import Chalice
+from chalice import Chalice # type: ignore
 
 app = Chalice(app_name='MuscleCore')
 
@@ -7,23 +7,3 @@ app = Chalice(app_name='MuscleCore')
 def index():
     return {'hello': 'world'}
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
